e4fc1e6a84ccfc36e31812e6710913d54d306062
Pragmatic <cryptopragmatic@protonmail.com>
2020-02-05 02:28:14 +0100

    /balance command - get node / bot waves balance

--- bot.go
+++ bot.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"strconv"
 	"strings"
 )
+
+const satInBtc = uint64(100000000)
 
 func executeBotCommand(tu TelegramUpdate) {
 	if strings.HasPrefix(tu.Message.Text, "/price") {
@@ -13,6 +16,8 @@ func executeBotCommand(tu TelegramUpdate) {
 		startCommand(tu)
 	} else if strings.HasPrefix(tu.Message.Text, "/address") {
 		addressCommand(tu)
+	} else if strings.HasPrefix(tu.Message.Text, "/balance") {
+		balanceCommand(tu)
 	} else {
 		unknownCommand(tu)
 	}
@@ -31,6 +36,14 @@ func startCommand(tu TelegramUpdate) {
 func addressCommand(tu TelegramUpdate) {
 	messageTelegram("My main Waves address is:", int64(tu.Message.Chat.ID))
 	messageTelegram(conf.NodeAddress, int64(tu.Message.Chat.ID))
+}
+
+func balanceCommand(tu TelegramUpdate) {
+	b, err := wnc.AddressesBalance(conf.NodeAddress)
+	if err != nil {
+		log.Printf("balanceCommand error: %s", err)
+	}
+	messageTelegram(fmt.Sprintf("My current Waves balance is: %.3f WAVES", float64(b.Balance)/float64(satInBtc)), int64(tu.Message.Chat.ID))
 }
 
 func unknownCommand(tu TelegramUpdate) {
