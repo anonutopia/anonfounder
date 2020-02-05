596da9df76d94949b04d86c920b65904bffddbd5
Pragmatic <cryptopragmatic@protonmail.com>
2020-02-05 01:45:57 +0100

    Bot work

--- bot.go
+++ bot.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func executeBotCommand(tu TelegramUpdate) {
+	if strings.HasPrefix(tu.Message.Text, "/price") {
+		priceCommand(tu)
+	} else if strings.HasPrefix(tu.Message.Text, "/start") {
+		startCommand(tu)
+	} else {
+		unknownCommand(tu)
+	}
+}
+
+func priceCommand(tu TelegramUpdate) {
+	kv := &KeyValue{Key: "currentPrice"}
+	db.First(kv, kv)
+	messageTelegram(fmt.Sprintf("Current token price is: %s €", strconv.Itoa(int(kv.ValueInt))), int64(tu.Message.Chat.ID))
+}
+
+func startCommand(tu TelegramUpdate) {
+	messageTelegram("Hello and welcome to Anonutopia!", int64(tu.Message.Chat.ID))
+}
+
+func unknownCommand(tu TelegramUpdate) {
+	messageTelegram("This command doesn't exist.", int64(tu.Message.Chat.ID))
+}
--- views.go
+++ views.go
@@ -1,16 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"strconv"
-
 	macaron "gopkg.in/macaron.v1"
 )
 
 func pageView(ctx *macaron.Context, tu TelegramUpdate) string {
-	kv := &KeyValue{Key: "currentPrice"}
-	db.First(kv, kv)
-	messageTelegram(fmt.Sprintf("%s €", strconv.Itoa(int(kv.ValueInt))), int64(tu.Message.Chat.ID))
+	executeBotCommand(tu)
 
 	return "OK"
 }
